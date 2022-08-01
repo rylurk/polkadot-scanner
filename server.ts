9882f278602a9c7435f1a7dc4a02129b9674cf12
ryan <ryanslh5@gmail.com>
2022-08-01 12:19:02 -0400

    Using Nodemon to work with TypeScript

--- server.ts
+++ server.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from 'express';
 import next from 'next';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  console.log('here', port);
 
   server.all('*', (req: Request, res: Response) => {
     return handle(req, res);
